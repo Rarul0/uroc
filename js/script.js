a33c753c1604235b5cba9292f47cfaec1d2c2b61
Rasul <rasy0@mail.ru>
2022-02-15 15:49:27 +0300

    one practice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 " use strict ";
+const numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели ?", "");
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    private: false,
+};
+
+const one = prompt ("Один из последних просмотренных фильмов?", "");
+      two = prompt ("На слолько оцените его",'');
+      tree =  prompt ("Один из последних просмотренных фильмов?", "");
+      four = prompt ("На слолько оцените его",'');
+
+
+      
+personalMovieDB.movies[one] = two;
+personalMovieDB.movies[tree] = four;
+
+console.log(personalMovieDB);
